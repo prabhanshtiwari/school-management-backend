b45e8665b14020d1f26fb228dffd65359373175c
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-09-02 09:44:10 +0000

    added express configurations and cors configurations

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// basic express configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+// sample route
+app.get("/", (req, res) => {
+  res.send("Hello from app.js");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
+import app from "./app.js";
+
 import dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
 });
 
-import express from "express";
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/instagram", (req, res) => {
-  res.send("Hello from instagram page!");
-});
 
 app.listen(port, () => {
   console.log(
